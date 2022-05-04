77043e9747af009c9c679fef03417f2295e9ff71
Erik Risholm <erik.risholm@edu.varberg.se>
2022-05-04 11:56:16 +0200

    links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {Routes, Route, Link} from 'react-router-dom';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="privacy" element={<Privacy/>}/>
+      </Routes>
     </div>
+  );
+}
+
+function Home(){
+  return(
+    <>
+    <nav>
+      <ul className="my-list">
+        <li>Home</li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/privacy">Privacy</Link></li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Here you get start getting to know me</h2>
+      <p>I really like en go whiskey and juicy hamburgers</p>
+    </main>
+    <section></section>
+    <footer></footer>
+    </>
+  );
+}
+function About(){
+  return(
+    <>
+    <nav>
+      <ul className="my-list">
+      <li><Link to="/home">Home</Link></li>
+        <li>About</li>
+        <li><Link to="/privacy">Privacy</Link></li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Here you get start getting to know me</h2>
+      <p>I really like en go whiskey and juicy hamburgers</p>
+    </main>
+    <section></section>
+    <footer></footer>
+    </>
+  );
+}
+function Privacy(){
+  return(
+    <>
+    <nav>
+      <ul className="my-list">
+      <li><Link to= "/home">Home</Link></li>
+        <li><Link to= "/about">About</Link></li>
+        <li>Privacy</li>
+      </ul>
+    </nav>
+    <main>
+      <h2>Here you get start getting to know me</h2>
+      <p>I really like en go whiskey and juicy hamburgers</p>
+    </main>
+    <section></section>
+    <footer></footer>
+    </>
   );
 }
 
